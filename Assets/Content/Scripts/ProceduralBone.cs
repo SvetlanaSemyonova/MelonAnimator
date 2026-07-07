47eafe690016f7d4c025767d3cfae26bddc0b719
Sviatlana Siamionava <antrapovichs@gmail.com>
2026-07-07 23:59:07 +0200

    Update current animations

--- Assets/Content/Scripts/ProceduralBone.cs
+++ Assets/Content/Scripts/ProceduralBone.cs
@@ -4,8 +4,30 @@ public class ProceduralBone : MonoBehaviour
 {
     public float restRotation;
 
+    private Rigidbody2D _body;
+    private Vector3 _restLocalPosition;
+    private Vector3 _pivot; 
+
+    private void Awake()
+    {
+        _body = GetComponent<Rigidbody2D>();
+        _restLocalPosition = transform.localPosition;
+
+        var hinge = GetComponent<HingeJoint2D>();
+        var distance = GetComponent<DistanceJoint2D>();
+        if (hinge != null) _pivot = hinge.anchor;
+        else if (distance != null) _pivot = distance.anchor;
+        else _pivot = Vector3.zero;
+    }
+
     private void LateUpdate()
     {
-        transform.localRotation = Quaternion.Euler(0f, 0f, restRotation);
+        if (_body != null && _body.simulated)
+            _body.simulated = false;
+
+        Quaternion rot = Quaternion.Euler(0f, 0f, restRotation);
+
+        transform.localRotation = rot;
+        transform.localPosition = _restLocalPosition + (_pivot - rot * _pivot);
     }
 }
--- Assets/Content/Scripts/ProceduralRig.cs
+++ Assets/Content/Scripts/ProceduralRig.cs
@@ -3,18 +3,24 @@ using UnityEngine;
 public class ProceduralRig : MonoBehaviour
 {
     private Rigidbody2D[] _bodies;
+    private ProceduralBone[] _bones;
 
     private void Awake()
     {
         _bodies = GetComponentsInChildren<Rigidbody2D>();
+        _bones = GetComponentsInChildren<ProceduralBone>();
         SetKinematic(true);
     }
 
     public void SetKinematic(bool kinematic)
     {
+        foreach (var bone in _bones)
+            bone.enabled = kinematic;
+
         foreach (var body in _bodies)
         {
             body.bodyType = kinematic ? RigidbodyType2D.Kinematic : RigidbodyType2D.Dynamic;
+            body.simulated = !kinematic;
         }
     }
 }
